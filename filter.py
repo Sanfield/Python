eb6778aa695d957268b8e48ab81db6746c1f2bb9
Sanfield <www.1042824123@qq.com>
2017-06-06 22:59:11 +0800

    内部函数

---
+++ filter.py
@@ -0,0 +1,5 @@
+def odd(x):
+    return x%2
+temp = range(10)
+show = filter(odd,temp)
+
---
+++ infunction.py
@@ -0,0 +1,6 @@
+def fun1():
+    print('fun1正在被调用')
+    def fun2():
+        print('fun2正在被调用')
+    fun2()
+    
