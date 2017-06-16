14f050d7c4b497ace3be5e355a5aa2fefb3011db
Sanfield <www.1042824123@qq.com>
2017-06-16 23:00:26 +0800

    新建一个类

---
+++ newclass.py
@@ -0,0 +1,12 @@
+#一个对象
+class Student:
+    name='Sanfield'
+    age='20'
+    sex='man'
+    school='xian technological university'
+    def read(self):
+        print('正在背四级作文')
+    def write(self):
+        print('正在写python')
+    def look(self):
+        print('正在看少帅')
