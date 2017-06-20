991e65020738f671efcd6354d880d507a2ae0dd5
Sanfield <www.1042824123@qq.com>
2017-06-20 23:30:20 +0800

    内置函数

---
+++ BIF1.py
@@ -0,0 +1,11 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:18:55) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: F:\python\object\issub.py =====================
+>>> issubclass(B,A)
+True
+>>> issubclass(B,B)
+True
+>>> issubclass(B,object)
+True
+>>> 
