ed8f44ca90782542e0cc45314b9de6a74ca1caf9
Sanfield <www.1042824123@qq.com>
2017-06-20 23:30:56 +0800

    判断是够是内置函数

---
+++ hasattr.py
@@ -0,0 +1,9 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:18:55) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: F:\python\object\issub.py =====================
+>>> c1= C()
+>>> hasattr(c1,'x')
+True
+>>>getattr(c1,'x')
+
