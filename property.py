9ac6fde4060360a57a6fb518a970036823b45b56
Sanfield <www.1042824123@qq.com>
2017-06-22 23:28:04 +0800

    property的相关方法

---
+++ property.py
@@ -0,0 +1,12 @@
+class C:
+    def __getattribute__(self,name):
+        print("getattribute")
+        return super().__getattribute__(name)
+    def __setattr__(self,name,value):
+        print("setattr")
+        super().__setattr__(name,value)
+    def __delattr__(self,name):
+        print("delattr")
+        super().__delattr__(name)
+    def __getattr__(self,name):
+        print("getattr")
